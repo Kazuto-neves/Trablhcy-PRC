50539b0d9c857243232c6d8568349478ad70cb1c
kazutolure <chrsmneves@gmail.com>
2020-10-05 14:17:11 -0300

    upgrade beta do ex3
    fazendo oque o proplema pede

--- exercicios/ex3.c
+++ exercicios/ex3.c
@@ -1,14 +1,28 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
 int main() {
-    int i,j=1;
+    int i;
     char f [100];
     printf("Escreva uma frase: ");
-    gets(f);                          
-    f[0] = toupper (f[0]); 
+    gets(f);
+    for(i=0;i<strlen(f);i++){
+        if (isalpha (f[0])){
+            f[0] = toupper (f[0]);
+            for(i=1;i<strlen(f);i++){
+                if(islower(f[i])&&(isspace (f[i-1]) || ispunct(f[i-1]))){
+                    f[i] = toupper (f[i]);
+                }
+            }
+        }else{
+            for(i=0;i<strlen(f);i++){
+                if(islower(f[i])&&(isspace (f[i-1]) || ispunct(f[i-1]))){
+                    f[i] = toupper (f[i]);
+                }
+            }
+        }
+    }
     printf("%s",f);
     return 0;
 }
