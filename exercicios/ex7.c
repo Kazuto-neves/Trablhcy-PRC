50ab859e32d3d4e81a1b05157b9f1d631cd80330
kazutolure <chrsmneves@gmail.com>
2020-10-12 19:41:20 -0300

    Update ex7.c

--- exercicios/ex7.c
+++ exercicios/ex7.c
@@ -3,51 +3,69 @@
 #include <string.h>
 #include <locale.h>
 
-typedef struct {
+typedef struct{
     char nome [30];
-    float n1,n2,media;
-}fAluno;
+    float n1,n2,media,acima,baixo;
+    
+}faluno;
 
 int compMedia(const void *fa,const void *fb){
-    fAluno a = *(fAluno*)fa;
-    fAluno b = *(fAluno*)fb;
+    faluno a = *(faluno*)fa;
+    faluno b = *(faluno*)fb;
     return ( b.media-a.media );
-}
-void lerD (fAluno n[], int N){
-    int i;
-    for (i=0;i<N; i++){
-        setbuf(stdin,NULL);
-        printf("\nDigite o nome: ");
-        gets(n[i].nome);
-        printf("\nDigite nota 1(%s): ",n[i].nome);
-        scanf("%f",&n[i].n1);
-        printf("\nDigite o nota 2(%s): ",n[i].nome);
-        scanf("%f",&n[i].n2);
-        printf("------------------------------\n");
-        setbuf(stdin,NULL);
-    }   
-}
-void calMedia(fAluno n[], int N){
-    int i;
-    for (i=0;i<N; i++){
-        n[i].media=(n[i].n1+(2*n[i].n2)/3);
-        }
-    }
-void mostrarD(fAluno n[], int N){
-    int i;
-    for(i=0; i<N; i++)
-	   printf("%-20s %05.2f\n",n[i].nome,n[i].media);
-	printf("\n\n\n");
 }
 int main(void) {
     setlocale(LC_ALL, "Portuguese");
-	fAluno alunos[100];
-    int N;
-    printf("Digite quantos alunos tem na turma: ");
-    scanf("%d",&N);
-	lerD(alunos,N);
-    calMedia(alunos,N);
-	qsort(alunos, N, sizeof(fAluno), compMedia);
-    mostrarD(alunos,N);
-	return 0;
+    faluno alunos[100];
+    faluno todos[100];
+    faluno abaixo[100];
+    int n,i,c=0,d=0;
+    double mediat;
+    printf("quantos alunos são: ");
+    scanf("%d", &n);
+    for(i=0;i<n;i++){
+        setbuf(stdin,NULL);
+        printf("nome do aluno(a):");
+        //scanf("%s",  alunos[i].nome);
+        gets(alunos[i].nome);
+        printf("nota1: ");
+        scanf("%f", &alunos[i].n1);
+        printf("nota2: ");
+        scanf("%f", &alunos[i].n2);
+        setbuf(stdin,NULL);
+    }
+      for(i=0;i<n;i++){
+          alunos[i].media=((alunos[i].n1+(2*alunos[i].n2))/3);
+          mediat+=alunos[i].media/n;
+      }
+      qsort(alunos, n, sizeof(faluno), compMedia);
+      for(i=0;i<n;i++){
+         if(alunos[i].media>mediat){
+             todos[c].media = alunos[i].media;
+             strcpy(todos[c].nome, alunos[i].nome);
+             todos[c].n1 = alunos[i].n1;
+             todos[c].n2 = alunos[i].n2;
+             c++;
+         }//else{
+             //abaixo[d].media = alunos[i].media;
+             //strcpy(abaixo[d].nome, abaixo[i].nome);
+             //abaixo[d].n1 = alunos[i].n1;
+            // abaixo[c].n2 = alunos[i].n2;
+             //d++;
+             
+         //}
+      }
+      printf("Media da turma: %05.2lf\n",mediat);
+      printf("----------------------------------------\n");
+      printf("|nome               |nota1|nota 2|media|\n");
+      printf("----------------------------------------\n");
+     for(i=0;i<n;i++) printf("|%-20s|%05.2f|%05.2f|%05.2f|\n", alunos[i].nome, alunos[i].n1, alunos[i].n2,alunos[i].media);
+    printf("----------------------------------------\n\n");
+    printf("os alunos acima da media da turma = %05.2lf\n",mediat);
+    printf("----------------------------------------\n");
+    printf("|nome               |nota1|nota 2|media|\n");
+    printf("----------------------------------------\n");
+    for(i=0;i<c;i++) printf("|%-20s|%05.2f|%05.2f|%05.2f|\n", todos[i].nome,todos[i].n1, todos[i].n2,todos[i].media);
+    printf("----------------------------------------\n");
+    return 0;
 }
